2cb87711bc61e35d26f9f9ba7b979476ee973ce1
Ryzen-71 <144885335+Ryzen-71@users.noreply.github.com>
2024-09-15 08:56:47 +0300

    Js на навигацию (#14)
    * Написал Js на навигацию
    
    * Сделал мобильную версию  главной страницы
    
    * Сделал главную страницу по Pixel Perfect
    
    * Сделал страницу каталога (мобильная версия) по Pixel Perfect
    
    * Сделал все страницы по Pixel Perfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navElement = document.querySelector('.main-header__nav');
+const buttonElement = document.querySelector('.main-header__toggler');
+
+navElement.classList.add('main-header__nav--closed');
+
+buttonElement.addEventListener('click', () => {
+  if (navElement.classList.contains('main-header__nav--closed')) {
+    navElement.classList.remove('main-header__nav--closed');
+    navElement.classList.add('main-header__nav--opened');
+  } else {
+    navElement.classList.remove('main-header__nav--opened');
+    navElement.classList.add('main-header__nav--closed');
+  }
+});
