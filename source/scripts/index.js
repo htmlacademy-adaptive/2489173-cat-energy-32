12fb6c84fed4a919f4547a7534c62082b83c21d7
Ryzen-71 <144885335+Ryzen-71@users.noreply.github.com>
2024-09-21 20:20:21 +0300

    Исправил недочеты (#15)
    * Испавил недочеты
    
    * Исправил недочеты по переполнению(1)
    
    * Добавил WebP-версии графики
    
    * Добавил nojs.Исправил недочеты в переполнении в навигании шапки
    
    * Иправил недочет по бэму
    
    * Убрал недочеты в в реализации nojs
    
    * Убрал недочеты по Pixel Perfect
    
    * Добавил noscript на карты
    
    * Добавил на интерактиную карту nojs
    
    * Внес поправки для проверки перед защитой
    
    * Убрал лишний ccs код.
    
    * Исправил недочеты для защиты
    
    * Исправляю стык в зеленом коте
    
    * Исправляю стык в зеленом фоне(2.0)
    
    * Отредактировал Js файлы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,23 @@
 /* в этот файл добавляет скрипты*/
+
 const navElement = document.querySelector('.main-header__nav');
 const buttonElement = document.querySelector('.main-header__toggler');
 
+
+navElement.classList.remove('main-header__nav--nojs');
 navElement.classList.add('main-header__nav--closed');
 
 buttonElement.addEventListener('click', () => {
-  if (navElement.classList.contains('main-header__nav--closed')) {
-    navElement.classList.remove('main-header__nav--closed');
-    navElement.classList.add('main-header__nav--opened');
-  } else {
+  if (navElement.classList.contains('main-header__nav--opened')) {
     navElement.classList.remove('main-header__nav--opened');
     navElement.classList.add('main-header__nav--closed');
+  } else {
+    navElement.classList.remove('main-header__nav--closed');
+    navElement.classList.add('main-header__nav--opened');
   }
 });
+
+/* в этот файл добавляет скрипт про интерактивную карту*/
+const contactsElement = document.querySelector('.contacts-map-script-wrap');
+
+contactsElement.classList.remove('contacts-map-script-wrap--nojs');
